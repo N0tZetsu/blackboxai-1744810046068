1a96b45f358f61def878d8d023caf3479bcf322c
N0tZetsu <emamainn@gmail.com>
2025-04-16 10:33:35 -0300

    Add fxmanifest.lua 1744810415957

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,21 +1,21 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'BlackboxAI'
+author 'TumberosHUD'
 description 'ESX Custom HUD with Money, Bank, ID, Hunger and Thirst'
 version '1.0.0'
 
-ui_page 'hud.html'
+ui_page 'html/index.html'
 
 client_scripts {
     'client.lua'
 }
 
 files {
-    'hud.html',
-    'style.css',
-    'script.js',
-    'img/*.png'
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js',
+    'html/images/*.png'
 }
 
 dependencies {
